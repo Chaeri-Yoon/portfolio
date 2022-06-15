97d5f0bad146952ba77fda9e36c3594984b656a9
Chaeri Yoon <cryoon0103@gmail.com>
2022-06-15 23:50:47 +0900

    * Pagination of Project Detail > About

--- src/sections/Project/ProjectDetail/About/data.tsx
+++ src/sections/Project/ProjectDetail/About/data.tsx
@@ -1,15 +1,41 @@
+import { ReactElement, useEffect, useRef } from "react";
 import styled from "styled-components";
+import PageButtons from "./PageButtons";
 
 const Video = styled.video`
     width: 100%;
     height: 100%;
+    background-color: black;
 `;
-
-const k21 = {
-    visual: (
-        <Video autoPlay>
-            <source src='/videos/projects/about/k21_1.mp4' />
-        </Video>
-    ),
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nibh purus, semper a congue egestas, convallis ac magna. Vivamus sit amet arcu at nisi ultricies molestie. Etiam lacinia lorem eu neque commodo hendrerit. Nullam placerat, magna ac aliquet fringilla, arcu purus convallis risus, vel consequat massa lacus pulvinar sapien. Nullam sit amet odio volutpat, dignissim nisi a, mattis arcu. Maecenas semper eros et mollis faucibus. Aenean sed mollis risus. Duis in pretium arcu, eu malesuada quam. Mauris et magna ut sem condimentum feugiat.'
+interface IData {
+    Visual: ReactElement;
+    description: string;
+}
+const K21 = (curPageNum: number): IData => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const visualSources = [
+        <source src='/videos/projects/about/K21_1.mp4' />,
+        <source src='/videos/projects/about/K21_2.mp4' />,
+        <source src='/videos/projects/about/K21_3.mp4' />
+    ];
+    const descriptionSources = [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Cras nibh purus, semper a congue egestas',
+        'magna ac aliquet fringilla, arcu purus convallis risus, vel consequat massa lacus pulvinar sapien.'
+    ];
+    useEffect(() => videoRef.current?.load(), [curPageNum]);
+    return {
+        Visual: (
+            <>
+                <Video autoPlay loop ref={videoRef}>
+                    {visualSources[curPageNum]}
+                </Video>
+                <PageButtons numOfPages={3} curPageNum={curPageNum} />
+            </>
+        ),
+        description: descriptionSources[curPageNum]
+    }
+}
+export default {
+    K21
 }
--- src/sections/Project/ProjectDetail/About/index.tsx
+++ src/sections/Project/ProjectDetail/About/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 import data from "./data";
 
@@ -10,8 +11,13 @@ const AboutContainer = styled.div`
     align-items: center;
 `;
 const AboutVisual = styled.div`
+    margin-bottom: 1em;
     width: 100%;
     aspect-ratio: 4 / 3;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
     & > video{
         width: 100%;
         height: 100%;
@@ -19,17 +25,31 @@ const AboutVisual = styled.div`
 `;
 const AboutDescription = styled.div`
     width: 100%;
-    aspect-ratio: 4 / 1.5;
+    aspect-ratio: 4 / 1;
+    border: 1px solid black;
+
     overflow-y: scroll;
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+    /* Hide scrollbar for Chrome, Safari and Opera */
+    &::-webkit-scrollbar{
+        display: none;
+    }
 `;
 export default ({ projectId }: { projectId: string }) => {
+    const [curPageNum, setCurPageNum] = useState(0);
+    const handlePageNumButton = (event: React.MouseEvent<HTMLElement>) => {
+        const { id } = event.target as HTMLElement;
+        if (!id.includes('page')) return;
+        setCurPageNum(+id.split('page')[1]);
+    };
     return (
         <AboutContainer>
-            <AboutVisual>
-                {projectId === 'k21' && data.k21.visual}
+            <AboutVisual onClick={handlePageNumButton}>
+                {projectId === 'K21' && data.K21(curPageNum).Visual}
             </AboutVisual>
             <AboutDescription>
-                <span>{projectId === 'k21' && data.k21.description}</span>
+                <span>{projectId === 'K21' && data.K21(curPageNum).description}</span>
             </AboutDescription>
         </AboutContainer>
     )
