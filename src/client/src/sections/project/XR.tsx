4c208202437a81b890fdbe42cfa9ed7a47c3c00b
Chaeri Yoon <cryoon0103@gmail.com>
2022-06-24 00:31:07 +0900

    Add thumbnails for XR projects

--- src/client/src/sections/project/XR.tsx
+++ src/client/src/sections/project/XR.tsx
@@ -3,10 +3,10 @@ import ProjectItem from "@sections/project/ProjectItem";
 export default () => {
     return (
         <>
-            <ProjectItem thumbnail='/projects/k21.png' projectName='K21'></ProjectItem>
-            <ProjectItem thumbnail='/projects/suspension.png' projectName='Suspension'></ProjectItem>
-            <ProjectItem thumbnail='/projects/crane.png' projectName='Crane'></ProjectItem>
-            <ProjectItem thumbnail='/projects/governor.png' projectName='Governor'></ProjectItem>
+            <ProjectItem thumbnail='/projects/XR/k21.png' projectName='K21'></ProjectItem>
+            <ProjectItem thumbnail='/projects/XR/suspension.png' projectName='Suspension'></ProjectItem>
+            <ProjectItem thumbnail='/projects/XR/crane.png' projectName='Crane'></ProjectItem>
+            <ProjectItem thumbnail='/projects/XR/governor.png' projectName='Governor'></ProjectItem>
         </>
     )
 }
